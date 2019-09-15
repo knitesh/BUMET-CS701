19bd778bc0dab090ce8f9d708a29874281f446a8
Kumar Nitesh <contact@kumar-nitesh.com>
2019-09-15 08:35:03 -0400

    Able to drop to correct location

--- Assignment2/HW2_Nitesh/code/partyWise.js
+++ Assignment2/HW2_Nitesh/code/partyWise.js
@@ -1,8 +1,15 @@
 (function() {
   const localstorage_key = "knitesh-senators-list";
 
-  var src, target, msg;
-  var sourceId;
+  const DEMOCRATE_PARTY = "Democrat";
+  const REPUBLICAN_PARTY = "Republican";
+
+  const msg = document.getElementById("msg");
+  const senatorSource = document.getElementById("members");
+  const democratesTarget = document.getElementById("democratsList");
+  const republicanTarget = document.getElementById("republicansList");
+
+  let sourceId, partyName;
   // XMLHttpRequest - asynchronous loading of XML data
   let xhr;
 
@@ -80,8 +87,8 @@
     const members = document.getElementById("members");
 
     const list = senators.map(senator => {
-      const key = senator.name.split(" ").join("-");
-      const el = `<li id=${key} class="collection-item" draggable="true">${senator.name}</li>`;
+      const key = senator.name.split(" ").join("");
+      const el = `<li id=${key} data-party=${senator.party} class="collection-item" draggable="true">${senator.name}</li>`;
 
       return el;
     });
@@ -92,4 +99,121 @@
   isSenatoresListInLocalStorage()
     ? getSenatorsListFromStorage()
     : makeXMLRequest("./partyList.xml");
+
+  function dragStartHandler(e) {
+    e.dataTransfer.setData("Text", e.target.id);
+    e.dataTransfer.setData("PartyName", e.target.getAttribute("data-party"));
+    sourceId = e.target.id; // explicitly for some browsers
+    partyName = e.target.getAttribute("data-party");
+    e.target.classList.add("dragged");
+  }
+
+  function dragEndHandler(e) {
+    msg.innerHTML = "Drag ended";
+    var elems = document.querySelectorAll(".dragged");
+    for (var i = 0; i < elems.length; i++) {
+      elems[i].classList.remove("dragged");
+    }
+  }
+
+  function dragHandler(e) {
+    msg.innerHTML =
+      "Dragging " +
+      e.target.id +
+      " party:" +
+      e.target.getAttribute("data-party");
+  }
+  // Democarte drop location handlers
+  function dDragEnterHandler(e) {
+    console.log(
+      "Drag Entering " +
+        e.target.id +
+        " source is " +
+        e.dataTransfer.getData("Text")
+    );
+
+    const partyAffiliation = e.dataTransfer.getData("PartyName") || partyName;
+    if (partyAffiliation == DEMOCRATE_PARTY) {
+      e.preventDefault();
+    }
+  }
+  function dDragOverHandler(e) {
+    console.log(
+      "Drag Over " +
+        e.target.id +
+        " source is " +
+        e.dataTransfer.getData("Text")
+    );
+
+    const partyAffiliation = e.dataTransfer.getData("PartyName") || partyName;
+    if (partyAffiliation == DEMOCRATE_PARTY) {
+      e.preventDefault();
+    }
+  }
+  function dDropHandler(e) {
+    e.preventDefault();
+    const sourceElementId = e.dataTransfer.getData("Text") || sourceId;
+    console.log("Drop on " + e.target.id + " source is " + sourceElementId);
+
+    var sourceElement = document.getElementById(sourceElementId);
+    var newElement = sourceElement.cloneNode(TextTrackCue);
+
+    democratesTarget.appendChild(newElement);
+    // republicanTarget.appendChild(newElement);
+  }
+
+  // Republican drop handlers
+  function rDragEnterHandler(e) {
+    console.log(
+      "Drag Entering " +
+        e.target.id +
+        " source is " +
+        e.dataTransfer.getData("Text")
+    );
+
+    const partyAffiliation = e.dataTransfer.getData("PartyName") || partyName;
+    if (partyAffiliation == REPUBLICAN_PARTY) {
+      e.preventDefault();
+    }
+  }
+  function rDragOverHandler(e) {
+    console.log(
+      "Drag Over " +
+        e.target.id +
+        " source is " +
+        e.dataTransfer.getData("Text")
+    );
+
+    const partyAffiliation = e.dataTransfer.getData("PartyName") || partyName;
+    if (partyAffiliation == REPUBLICAN_PARTY) {
+      e.preventDefault();
+    }
+  }
+
+  function rDropHandler(e) {
+    e.preventDefault();
+    const sourceElementId = e.dataTransfer.getData("Text") || sourceId;
+    console.log("Drop on " + e.target.id + " source is " + sourceElementId);
+
+    var sourceElement = document.getElementById(sourceElementId);
+    var newElement = sourceElement.cloneNode(TextTrackCue);
+
+    republicanTarget.appendChild(newElement);
+    // republicanTarget.appendChild(newElement);
+  }
+
+  // Add event handlers for the source
+  senatorSource.ondragstart = dragStartHandler;
+  senatorSource.ondragend = dragEndHandler;
+  senatorSource.ondrag = dragHandler;
+
+  // Add event handlers for the Democrates target
+  democratesTarget.ondragenter = dDragEnterHandler;
+  democratesTarget.ondragover = dDragOverHandler;
+  democratesTarget.ondrop = dDropHandler;
+
+  // Add event handlers for the Republican target
+  republicanTarget.ondragenter = rDragEnterHandler;
+  republicanTarget.ondragover = rDragOverHandler;
+  republicanTarget.ondrop = rDropHandler;
 })();
