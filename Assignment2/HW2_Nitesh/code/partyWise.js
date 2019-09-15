7c18e5261b5c05fe285466e8b3924853390ef67f
Kumar Nitesh <contact@kumar-nitesh.com>
2019-09-14 22:26:48 -0400

    displaying partywise

--- Assignment2/HW2_Nitesh/code/partyWise.js
+++ Assignment2/HW2_Nitesh/code/partyWise.js
@@ -1,7 +1,10 @@
 (function() {
+  const localstorage_key = "knitesh-senators-list";
+
+  var src, target, msg;
+  var sourceId;
   // XMLHttpRequest - asynchronous loading of XML data
   let xhr;
-  let senators = [];
 
   const makeXMLRequest = url => {
     if (window.XMLHttpRequest) {
@@ -22,18 +25,21 @@
 
   // callback function when data is loaded
   const loadXMLData = () => {
+    let senators = [];
     if (xhr.readyState == 4) {
       if (xhr.status == 200) {
         // get all the senator elements
-        var allSongs = xhr.responseXML.getElementsByTagName("senator");
-        for (var i = 0; i < allSongs.length; i++) {
-          var name = allSongs[i].getElementsByTagName("name")[0].textContent;
-          var party = allSongs[i].getElementsByTagName("party")[0].textContent;
+        var allSenators = xhr.responseXML.getElementsByTagName("senator");
+        for (var i = 0; i < allSenators.length; i++) {
+          let name = allSenators[i].getElementsByTagName("name")[0].textContent;
+          let party = allSenators[i].getElementsByTagName("party")[0]
+            .textContent;
 
           // create a new JSON object for each song
-          var newSenator = {
+          let newSenator = {
             name,
-            party
+            party,
+            voted: false
           };
           // add the object to the array
           senators.push(newSenator);
@@ -41,6 +47,9 @@
 
         document.getElementById("status").innerHTML =
           "Loaded " + senators.length + " senators";
+
+        // insert data into
+        insertSenatorsListIntoLocalStorage(senators);
       } else {
         document.getElementById("status").innerHTML =
           "There was a problem with the request " + xhr.status;
@@ -48,6 +57,35 @@
     }
   };
 
+  const isSenatoresListInLocalStorage = () => {
+    const data = localStorage.getItem(localstorage_key);
+    console.log(data && data.length > 0);
+    return data && data.length > 0 ? true : false;
+  };
+
+  const insertSenatorsListIntoLocalStorage = senators => {
+    localStorage.setItem(localstorage_key, JSON.stringify(senators));
+    displaySenatorsList();
+  };
+  const getSenatorsListFromStorage = () => {
+    const list = localStorage.getItem(localstorage_key);
+    return JSON.parse(list);
+  };
+
+  const displaySenatorsList = () => {
+    const members = document.getElementById("members");
+    const senators = getSenatorsListFromStorage();
+    const list = senators.map(senator => {
+      const key = senator.name.split(" ").join("-");
+      const el = `<li id=${key} class="collection-item" draggable="true">${senator.name}</li>`;
+
+      return el;
+    });
+
+    members.innerHTML = list.join("");
+  };
   // read data from XML
-  makeXMLRequest("./partyList.xml");
+  isSenatoresListInLocalStorage()
+    ? displaySenatorsList()
+    : makeXMLRequest("./partyList.xml");
 })();
