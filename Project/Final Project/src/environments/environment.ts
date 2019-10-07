45d79c4104e8c1293c58620d0e4b18a2c4bda277
Kumar Nitesh <contact@kumar-nitesh.com>
2019-10-06 20:28:39 -0400

    MapQuest

---
+++ Project/Final Project/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA5oxl7cibZ_FomInDs8RlsuD8UmpVeNr8",
+    authDomain: "ng-collectorjs.firebaseapp.com",
+    databaseURL: "https://ng-collectorjs.firebaseio.com",
+    projectId: "ng-collectorjs",
+    storageBucket: "",
+    messagingSenderId: "888721129443",
+    appId: "1:888721129443:web:2dfa5538cf12d1ade32ad6",
+    measurementId: "G-XZ9E6RGQFT"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
